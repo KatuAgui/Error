98e7f947249b56e1dea3a72fc8ed004d9de90c78
Admi <Admi@DESKTOP-46CR48Q>
2021-04-26 06:58:46 +0200

    Primer commit

---
+++ src/main/java/com/curlp/capadatos/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.capadatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Admi
+ */
+public class Conexion {
+    private static String url = "jdbc:mysql://localhost:3306/sistemaclinicooficial?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static String user = "root";
+    private static String clave = "Yamileth1.";
+    
+    public static Connection conectar() throws SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //obtener la cadena de conexion
+            return DriverManager.getConnection(url, user, clave);
+            
+        } catch (ClassNotFoundException e){
+            throw new SQLException(e.getMessage());
+    
+    }
+    }
+    
+}
