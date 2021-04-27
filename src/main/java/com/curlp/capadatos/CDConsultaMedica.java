8a0b7f0fe5750524c536a1f2c654ce064f914fc4
Willian Josue <wjoe1995@live.com>
2021-04-26 23:19:20 -0600

    Trabaje en la CapaLogica y parte de la CapadeDatos

--- src/main/java/com/curlp/capadatos/CDConsultaMedica.java
+++ src/main/java/com/curlp/capadatos/CDConsultaMedica.java
@@ -5,10 +5,84 @@
  */
 package com.curlp.capadatos;
 
+import com.curlp.capalogica.CLConsultaMedica;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admi
  */
 public class CDConsultaMedica {
     
+    private final Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    public CDConsultaMedica() throws SQLException{
+        this.cn = Conexion.conectar();
+    }
+    
+    // metodo para insertar una ConsultaMedica
+    public void insertarConsulta(CLConsultaMedica cl) throws SQLException{
+  
+        String sql = "{CALL sp_insertarConsultaMedica(?,?,?,?,?)}";
+        
+        try {
+           ps = cn.prepareCall(sql);
+           ps.setString(1, cl.getFechaIngreso());
+           ps.setString(2, cl.getObservaciones());
+           ps.setString(3, cl.getRecetasMedicas());
+           ps.setString(4, cl.getNumeroIdentidad());
+           ps.setInt(5, cl.getIdUsuario());
+           ps.execute();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }  
+    }
+    
+    public void actualizarConsulta(CLConsultaMedica cl) throws SQLException{
+        String sql = "{CALL sp_actualizarConsultaMedica(?,?,?,?,?)}";
+        
+        try {
+           ps = cn.prepareCall(sql);
+           ps.setString(1, cl.getFechaIngreso());
+           ps.setString(2, cl.getObservaciones());
+           ps.setString(3, cl.getRecetasMedicas());
+           ps.setString(4, cl.getNumeroIdentidad());
+           ps.setInt(5, cl.getIdUsuario());
+           ps.execute();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }
+    }
+    
+    public void eliminarConsulta(CLConsultaMedica cl) throws  SQLException{
+        String sql = "{CALL sp_eliminarConsulta(?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.execute();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }
+    }
+    
+    public void mostrarConsulta(CLConsultaMedica cl) throws  SQLException{
+        String sql = "{CALL sp_mostrarConsulta()}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.execute();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }
+    
+    }
 }
--- src/main/java/com/curlp/capalogica/CLConsultaMedica.java
+++ src/main/java/com/curlp/capalogica/CLConsultaMedica.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.curlp.capalogica;
 
-/**
- *
- * @author Admi
- */
 public class CLConsultaMedica {
+    
+    private int idConsultasMedicas;
+    private String fechaIngreso;
+    private String observaciones;
+    private String recetasMedicas;
+    private String numeroIdentidad;
+    private int idUsuario;
+
+    public int getIdConsultasMedicas() {
+        return idConsultasMedicas;
+    }
+
+    public void setIdConsultasMedicas(int idConsultasMedicas) {
+        this.idConsultasMedicas = idConsultasMedicas;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public String getRecetasMedicas() {
+        return recetasMedicas;
+    }
+
+    public void setRecetasMedicas(String recetasMedicas) {
+        this.recetasMedicas = recetasMedicas;
+    }
+
+    public String getNumeroIdentidad() {
+        return numeroIdentidad;
+    }
+
+    public void setNumeroIdentidad(String numeroIdentidad) {
+        this.numeroIdentidad = numeroIdentidad;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
     
 }
