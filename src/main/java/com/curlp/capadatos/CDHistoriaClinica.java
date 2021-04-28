3ab23802ff2b11f52f232382b9ca47b5cc692e70
hnunez <hnunez@DESKTOP-3LGF3TS>
2021-04-28 00:45:02 -0600

    Agrego Historia Clinica Capa Logica y Capa Datos

--- src/main/java/com/curlp/capadatos/CDHistoriaClinica.java
+++ src/main/java/com/curlp/capadatos/CDHistoriaClinica.java
@@ -5,10 +5,104 @@
  */
 package com.curlp.capadatos;
 
+import com.curlp.capalogica.CLHistoriaClinica;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admi
  */
 public class CDHistoriaClinica {
+    //Declarar variables de coneccion de consultas
+    private final Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    Statement st;
+
+     
+    public CDHistoriaClinica() throws SQLException {
+        this.cn = Conexion.conectar();
+    }
+    
+    //Metodo para insertar una Historia Clinica
+    public void insertarHistoriaClinica (CLHistoriaClinica cl) throws SQLException {
+    
+         String sql = "{CALL sp_insertarHistoriaMedica(?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        
+        try{
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.setString(2, cl.getFechaCreacion());
+            ps.setString(3,cl.getCardiobasculares());
+            ps.setString(4, cl.getPulmonares());
+            ps.setString(5, cl.getDigestivo());
+            ps.setString(6, cl.getDiavetes());
+            ps.setString(7, cl.getRenales());
+            ps.setString(8, cl.getQuirurgicos());
+            ps.setString(9, cl.getAlergicos());
+            ps.setString(10, cl.getTransfusiones());   
+            ps.setString(11, cl.getMedicamentos());  
+            ps.setString(12, cl.getObservaciones());  
+            ps.setInt(13, cl.getIdUsuario());  
+            ps.execute();
+           
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+    }
+    
+    //Metodo para actualizar Historia Clinica
+    public void actualizarHistoriaMedica(CLHistoriaClinica cl) {
+      
+       String sql = "{CALL sp_actualizarHistoriaMedica(?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        
+        try{
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.setString(2, cl.getFechaCreacion());
+            ps.setString(3,cl.getCardiobasculares());
+            ps.setString(4, cl.getPulmonares());
+            ps.setString(5, cl.getDigestivo());
+            ps.setString(6, cl.getDiavetes());
+            ps.setString(7, cl.getRenales());
+            ps.setString(8, cl.getQuirurgicos());
+            ps.setString(9, cl.getAlergicos());
+            ps.setString(10, cl.getTransfusiones());   
+            ps.setString(11, cl.getMedicamentos());  
+            ps.setString(12, cl.getObservaciones());  
+            ps.setInt(13, cl.getIdUsuario());  
+            ps.execute();
+           
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+    
+    }
+    
+    //Metodo para eliminar Historia Clinica
+    
+    public void eliminarHistoria(CLHistoriaClinica cl) throws  SQLException{
+        String sql = "{CALL sp_eliminarHistoriaClinica(?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.execute();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }
+    }
+    
+    
+    
     
 }
--- src/main/java/com/curlp/capalogica/CLHistoriaClinica.java
+++ src/main/java/com/curlp/capalogica/CLHistoriaClinica.java
@@ -10,5 +10,126 @@ package com.curlp.capalogica;
  * @author Admi
  */
 public class CLHistoriaClinica {
+    private String numeroIdentidad;
+    private String fechaCreacion;
+    private String cardiobasculares;
+    private String pulmonares;
+    private String digestivo;
+    private String diavetes;
+    private String renales;
+    private String quirurgicos;
+    private String alergicos;
+    private String transfusiones;
+    private String medicamentos;
+    private String observaciones;
+    private int idUsuario;
+
+    public String getNumeroIdentidad() {
+        return numeroIdentidad;
+    }
+
+    public void setNumeroIdentidad(String numeroIdentidad) {
+        this.numeroIdentidad = numeroIdentidad;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getCardiobasculares() {
+        return cardiobasculares;
+    }
+
+    public void setCardiobasculares(String cardiobasculares) {
+        this.cardiobasculares = cardiobasculares;
+    }
+
+    public String getPulmonares() {
+        return pulmonares;
+    }
+
+    public void setPulmonares(String pulmonares) {
+        this.pulmonares = pulmonares;
+    }
+
+    public String getDigestivo() {
+        return digestivo;
+    }
+
+    public void setDigestivo(String digestivo) {
+        this.digestivo = digestivo;
+    }
+
+    public String getDiavetes() {
+        return diavetes;
+    }
+
+    public void setDiavetes(String diavetes) {
+        this.diavetes = diavetes;
+    }
+
+    public String getRenales() {
+        return renales;
+    }
+
+    public void setRenales(String renales) {
+        this.renales = renales;
+    }
+
+    public String getQuirurgicos() {
+        return quirurgicos;
+    }
+
+    public void setQuirurgicos(String quirurgicos) {
+        this.quirurgicos = quirurgicos;
+    }
+
+    public String getAlergicos() {
+        return alergicos;
+    }
+
+    public void setAlergicos(String alergicos) {
+        this.alergicos = alergicos;
+    }
+
+    public String getTransfusiones() {
+        return transfusiones;
+    }
+
+    public void setTransfusiones(String transfusiones) {
+        this.transfusiones = transfusiones;
+    }
+
+    public String getMedicamentos() {
+        return medicamentos;
+    }
+
+    public void setMedicamentos(String medicamentos) {
+        this.medicamentos = medicamentos;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    
+    
+            
     
 }
