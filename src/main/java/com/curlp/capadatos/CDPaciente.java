d6e6079a68c0adb3c1410baa60623f1a8b365cc1
nesto <nesto@192.168.0.17>
2021-04-28 13:25:52 -0600

    CLPaciente y CDPaciente

--- src/main/java/com/curlp/capadatos/CDPaciente.java
+++ src/main/java/com/curlp/capadatos/CDPaciente.java
@@ -3,12 +3,158 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.curlp.capadatos;
 
-/**
- *
- * @author Admi
- */
+import com.curlp.capalogica.CLPaciente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
 public class CDPaciente {
+
+private final Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    Statement st;
+
+    public CDPaciente() throws SQLException {
+        this.cn = Conexion.conectar();
+    }
+    public void insertarPaciente(CLPaciente cl) throws SQLException{
+        String sql = "{CALL sp_insertarPaciente(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        try{
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.setString(2, cl.getPrimerNombre());
+            ps.setString(3,cl.getSegundoNombre());
+            ps.setString(4, cl.getPrimerApellido());
+            ps.setString(5, cl.getSegundoApellido());
+            ps.setString(6, cl.getAntecedentesFamiliares());
+            ps.setString(7, cl.getFechaNacimiento());
+            ps.setString(8, cl.getTipoSangre());
+            ps.setString(9, cl.getDireccion());
+            ps.setString(10, cl.getTelefonoCelular());
+            ps.setDouble(11, cl.getPeso());
+            ps.setDouble(12, cl.getEstatura());
+            ps.setString(13, cl.getCiudadProcedencia());
+            ps.setString(14, cl.getEmail());
+            ps.setInt(15, cl.getIdSexo());
+            ps.execute();
+           
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+    }
     
+    public void actualizarPaciente(CLPaciente cl) {
+        String sql = "{CALL sp_actualizarPaciente(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        try{
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.setString(2, cl.getPrimerNombre());
+            ps.setString(3,cl.getSegundoNombre());
+            ps.setString(4, cl.getPrimerApellido());
+            ps.setString(5, cl.getSegundoApellido());
+            ps.setString(6, cl.getAntecedentesFamiliares());
+            ps.setString(7, cl.getFechaNacimiento());
+            ps.setString(8, cl.getTipoSangre());
+            ps.setString(9, cl.getDireccion());
+            ps.setString(10, cl.getTelefonoCelular());
+            ps.setDouble(11, cl.getPeso());
+            ps.setDouble(12, cl.getEstatura());
+            ps.setString(13, cl.getCiudadProcedencia());
+            ps.setString(14, cl.getEmail());
+            ps.setInt(15, cl.getIdSexo());
+            ps.execute();
+           
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+    
+    }
+    
+    public void eliminarPaciente(CLPaciente cl) throws SQLException{
+        String sql = "{CALL sp_eliminarPaciente(?)}";
+        
+        try{
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNumeroIdentidad());
+            ps.execute();
+           
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+    
+    }
+    
+ //Metodo para poblar tabla
+    public List<CLPaciente> obtenerListaPaciente() throws SQLException{
+        String sql = "{CALL sp_mostrarPaciente()}";
+        List<CLPaciente> miLista = null;
+        try{
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            miLista = new ArrayList<>();
+            while(rs.next()) {
+                CLPaciente cl = new CLPaciente();
+                cl.setNumeroIdentidad(rs.getString("numeroIdentidad"));
+                cl.setPrimerNombre(rs.getString("primerNombre"));
+                cl.setSegundoNombre(rs.getString(rs.getString("segundoNombre")));
+                cl.setPrimerApellido(rs.getString("primerApellido"));
+                cl.setSegundoApellido(rs.getString(rs.getString("segundoApellido")));
+                cl.setAntecedentesFamiliares(rs.getNString("antecedentesFamiliares"));
+                cl.setFechaNacimiento(rs.getString("fechaNacimiento"));
+                cl.setTipoSangre(rs.getString("tipoSangre"));
+                cl.setDireccion(rs.getString("direccion"));
+                cl.setTelefonoCelular(rs.getString("telefonoCelular"));
+                cl.setPeso(rs.getDouble("peso"));
+                cl.setEstatura(rs.getDouble("estatura"));
+                cl.setCiudadProcedencia(rs.getString("ciudadProcedencia"));
+                cl.setEmail(rs.getString("email"));
+                cl.setIdSexo(rs.getInt("idSexo"));
+                miLista.add(cl);
+            }
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+        
+        }
+        return miLista;
+        
+    }
+    //Metodo comboPacientes
+   public List<String> cargarComboPacientes() throws SQLException{
+        String sql = "{CALL sp_mostrarPaciente()}";
+        
+        List<String> miLista = null;
+   
+        try{
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            miLista = new ArrayList<>();
+            miLista.add("--Seleccione--");
+            while (rs.next()){
+                miLista.add(rs.getString("numeroIdentidad"));
+            
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "error: "+ e.getMessage());
+            
+            
+        }
+        return miLista;
+    }    
 }
--- src/main/java/com/curlp/capalogica/CLPaciente.java
+++ src/main/java/com/curlp/capalogica/CLPaciente.java
@@ -10,5 +10,141 @@ package com.curlp.capalogica;
  * @author Admi
  */
 public class CLPaciente {
+    private String numeroIdentidad;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String antecedentesFamiliares;
+    private String fechaNacimiento;
+    private String tipoSangre;
+    private String direccion;
+    private String telefonoCelular;
+    private double peso;
+    private double estatura;
+    private String ciudadProcedencia;
+    private String email;
+    private int idSexo;
+
+    public String getNumeroIdentidad() {
+        return numeroIdentidad;
+    }
+
+    public void setNumeroIdentidad(String numeroIdentidad) {
+        this.numeroIdentidad = numeroIdentidad;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getAntecedentesFamiliares() {
+        return antecedentesFamiliares;
+    }
+
+    public void setAntecedentesFamiliares(String antecedentesFamiliares) {
+        this.antecedentesFamiliares = antecedentesFamiliares;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefonoCelular() {
+        return telefonoCelular;
+    }
+
+    public void setTelefonoCelular(String telefonoCelular) {
+        this.telefonoCelular = telefonoCelular;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getCiudadProcedencia() {
+        return ciudadProcedencia;
+    }
+
+    public void setCiudadProcedencia(String ciudadProcedencia) {
+        this.ciudadProcedencia = ciudadProcedencia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(int idSexo) {
+        this.idSexo = idSexo;
+    }
+    
     
 }
